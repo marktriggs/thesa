536fe010a727340af202027c3d8041f13123b141
Mark Triggs <mark@dishevelled.net>
2015-10-05 09:12:52 +1100

    Defined permissions

---
+++ backend/plugin_init.rb
@@ -0,0 +1,7 @@
+Permission.define("update_thesas_record",
+                  "The ability to create and update thesas records",
+                  :level => "global")
+
+Permission.define("delete_thesas_record",
+                  "The ability to delete thesas records",
+                  :level => "global")
