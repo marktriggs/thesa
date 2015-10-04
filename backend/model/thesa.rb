4f8220befa9cd86de5bd3be2106406cad6734b62
Mark Triggs <mark@dishevelled.net>
2015-10-05 09:08:37 +1100

    Generate a display string for each Thesa record

--- backend/model/thesa.rb
+++ backend/model/thesa.rb
@@ -6,4 +6,23 @@ class Thesa < Sequel::Model(:thesa)
 
   set_model_scope :repository
 
+
+  def self.sequel_to_jsonmodel(objs, opts = {})
+    jsons = super
+
+    jsons.zip(objs).each do |json, obj|
+      p json
+      subject = json['subject']
+      terms = ASUtils.wrap(json['thesa_terms']).map {|term| term['thesa_term']}.join(" -- ")
+
+      json['display_string'] = subject
+
+      if !terms.empty?
+        json['display_string'] += " -- " + terms
+      end
+    end
+
+    jsons
+  end
+
 end
