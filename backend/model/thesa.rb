9d3591dd4535238889fd9df232d46edf2f14c708
Mark Triggs <mark@dishevelled.net>
2015-10-09 08:18:24 +1100

    Fix the display of terms

--- backend/model/thesa.rb
+++ backend/model/thesa.rb
@@ -13,12 +13,12 @@ class Thesa < Sequel::Model(:thesa)
     jsons.zip(objs).each do |json, obj|
       p json
       subject = json['subject']
-      terms = ASUtils.wrap(json['thesa_terms']).map {|term| term['thesa_term']}.join(" -- ")
+      terms = ASUtils.wrap(json['thesa_terms']).map {|term| term['term']}.join(" -- ")
 
       json['display_string'] = subject
 
       if !terms.empty?
-        json['display_string'] += " -- " + terms
+        json['display_string'] += " -- #{terms}"
       end
     end
 
